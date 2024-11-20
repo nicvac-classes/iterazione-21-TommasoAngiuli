0354e0fc6b6f9f047c5cbbf833ab5f008196c2a7
TommasoAngiuli <tommaso.angiuli@marconibari.edu.it>
2024-11-20 22:15:33 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int bambini, adulti, postiTot;
+    float occupati, daLiberare;
+    postiTot = 30;
+    occupati = 0;
+    do {
+        cout << "quanti adulti salgono?" << endl;
+        cin >> adulti;
+        cout << "quanti bambini salgono?" << endl;
+        cin >> bambini;
+        occupati = occupati + adulti + (bambini / 2);
+    } while (occupati < postiTot);
+    if (occupati > postiTot) {
+        daLiberare = occupati - postiTot;
+        cout << "da liberare " << daLiberare << " posti" << endl;
+    }
+    cout << "la giostra può partire" << endl;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
